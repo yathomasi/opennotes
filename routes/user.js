d7503c8eaa242d04a989d474360c584db64f8390
Thomas Kunwar <yathomasi@gmail.com>
2020-01-03 16:59:43 +0545

    using auth middleware to list all users

--- routes/user.js
+++ routes/user.js
@@ -4,9 +4,10 @@ const {
   registerValidation,
   validate
 } = require("../middleware/validator");
+const auth = require("../middleware/auth");
 
 module.exports = server => {
-  server.get("/api/v1/users", userController.listUsers);
+  server.get("/api/v1/users", auth.authJWT, userController.listUsers);
   server.get("/api/v1/users/:id", userController.getUser);
   server.post(
     "/api/v1/register",
