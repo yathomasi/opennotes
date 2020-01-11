fadb56fdd33c35fb7e31661cdfff0b8823826e67
Thomas Kunwar <yathomasi@gmail.com>
2020-01-11 15:37:21 +0545

    added / route to test

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const userRoutes = require("./user");
 const noteRoutes = require("./note");
 
 module.exports = server => {
+  server.get("/", indexController.home);
   server.get("/api/v1/test", indexController.home);
   userRoutes(server);
   noteRoutes(server);
